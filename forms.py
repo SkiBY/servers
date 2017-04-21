00857a2a66bfb36ffb0e7458b003edf464e88e95
Sergei Ruzki <sergei.ruzki@gmail.com>
2017-04-21 21:21:30 +0300

    структура проекта, инструкция

--- forms.py
+++
@@ -1,7 +0,0 @@
-from flask.ext.wtf import Form
-from wtforms import TextField, BooleanField
-from wtforms.validators import Required
-
-class LoginForm(Form):
-    login = TextField('login', validators = [Required()])
-    password = TextField('password', validators = [Required()])
